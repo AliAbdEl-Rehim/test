242a7690f9da18aa57a6dc01f4ff359ec4c06919
mahmoud-abd-elrehim <m.abdelrehim92@gmail.com>
2025-05-09 21:17:20 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'inventory', pathMatch: 'full' },
+  { path: 'inventory', loadChildren: () => import('./features/inventory/inventory.module').then(m => m.InventoryModule) }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
